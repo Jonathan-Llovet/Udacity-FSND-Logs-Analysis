e553e47804930dcf77e72669cda75c5b5ae0733f
Jonathan Llovet <Jonathan.Llovet@gmail.com>
2018-11-26 20:59:27 -0500

    Make python script pep8 compliant

--- vagrant/log_analysis.py
+++ vagrant/log_analysis.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.5.2
 
 import psycopg2
+
 
 def report_logs(queries, query_names):
     '''Generates blog traffic report'''
@@ -15,6 +16,7 @@ def report_logs(queries, query_names):
                 print("{} - {}".format(result[i][0], result[i][1]))
         print("\n")
 
+
 def run_SELECT_query(query, db="news"):
     """Connect to the database and run the query specified"""
     db = "dbname={}".format(db)
@@ -25,11 +27,15 @@ def run_SELECT_query(query, db="news"):
     cur.close()
     conn.close()
     return query_result
-    
+
+
 def name_queries():
     """Create a list of titles for queries that are going to be run"""
-    query_names = ["The Most Popular Articles of All Time", "The Most Popular Authors of All Time", "Days With Connection Issues"]
+    query_names = ["The Most Popular Articles of All Time",
+                   "The Most Popular Authors of All Time",
+                   "Days With Connection Issues"]
     return query_names
+
 
 def create_queries(query_names):
     """Create a dictionary of queries to be run"""
@@ -37,9 +43,9 @@ def create_queries(query_names):
     # Create a query to select the 3 most popular articles of all time
     queries.append("""
         SELECT r.title, COUNT(l.path)::TEXT || ' views' AS popular_articles
-        FROM articles AS r 
-        INNER JOIN log AS l 
-            ON r.slug = RIGHT(l.path,LENGTH(r.slug))
+        FROM articles AS r
+        INNER JOIN log AS l
+        ON r.slug = RIGHT(l.path,LENGTH(r.slug))
         GROUP BY r.title
         ORDER BY COUNT(l.path) DESC
         LIMIT 3;
@@ -49,31 +55,33 @@ def create_queries(query_names):
     queries.append("""
         SELECT a.name, count(l.path) || ' views' AS calls
         FROM log AS l
-        INNER JOIN 
-            (SELECT a.name AS name, art.title AS title, art.slug AS slug
-            FROM authors AS a
-            INNER JOIN articles AS art
-            ON art.author = a.id
-            ) AS a
-            ON a.slug = RIGHT(l.path,LENGTH(a.slug))
+        INNER JOIN
+        (SELECT a.name AS name, art.title AS title, art.slug AS slug
+        FROM authors AS a
+        INNER JOIN articles AS art
+        ON art.author = a.id
+        ) AS a
+        ON a.slug = RIGHT(l.path,LENGTH(a.slug))
         GROUP BY a.name
         ORDER BY calls DESC
         LIMIT 5;
         """)
 
-    # Create a query to select days where more than 1% of server requests resulted in errors
+    # Create a query to select days where more than 1% of
+    # server requests resulted in errors
     queries.append("""
-        SELECT de.day, ROUND((num_errors::NUMERIC / requests::NUMERIC * 100),1) AS perc_error
+        SELECT de.day, ROUND((num_errors::NUMERIC / requests::NUMERIC * 100),1)
+        AS perc_error
         FROM log
         INNER JOIN daily_errors as de
-            ON TO_CHAR(DATE_TRUNC('day', log.time),'FMMonth, DD, YYYY') = de.day
+        ON TO_CHAR(DATE_TRUNC('day', log.time),'FMMonth, DD, YYYY') = de.day
         INNER JOIN daily_requests AS dr
-            ON de.day = dr.day
+        ON de.day = dr.day
         WHERE (num_errors::NUMERIC / requests::NUMERIC * 100) > 1
         GROUP BY de.day, perc_error
         ORDER BY de.day;
         """)
-    
+
     query_results = dict(zip(query_names, queries))
     return query_results
 
